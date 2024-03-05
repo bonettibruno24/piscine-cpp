7eb09b33b01c336797735b86009b0af81853c70c
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-05 09:35:03 -0300

    feat: create variables the values

---
+++ methods.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Mundo!\nHello C++!\n";
+
+    system("pause");
+    return 0;
+}
+
+// variavéis 
+
+int main() {
+    // TIPO NOME
+    // TIPO NOME = VALOR
+    int vidas;  // 10, 20, 30
+    char letra; // 'a', 'b'
+    char letras [20]; // 'a', 'b', 'c' 
+    double decimal; //2.499999
+    float decimal2; //2.5
+    bool vivo; // true / false  . 0 e 1 
+    string nome; //  "mundo"
+
+    
+
+    return 0;
+}
+
+
+
