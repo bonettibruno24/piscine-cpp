a2ad9e2592a3d48e232180698fed01fc5302644d
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-05 10:38:03 -0300

    feat: imprimit value in variable, activity home

--- methods.cpp
+++ methods.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 using namespace std;
+// int main() {
+//     cout << "Hello Mundo!\nHello C++!\n";
 
-int main() {
-    cout << "Hello Mundo!\nHello C++!\n";
-
-    system("pause");
-    return 0;
-}
-
-// variavéis 
+//     system("pause");
+//     return 0;
+// }
+// variavéis
 
 int main() {
     // TIPO NOME
     // TIPO NOME = VALOR
-    int vidas;  // 10, 20, 30
-    char letra; // 'a', 'b'
-    char letras [20]; // 'a', 'b', 'c' 
-    double decimal; //2.499999
-    float decimal2; //2.5
-    bool vivo; // true / false  . 0 e 1 
-    string nome; //  "mundo"
 
-    
+    int vidas = 0;  // 10, 20, 30
+    char letra='B'; // 'a', 'b'
+    char letras [20]; // 'a', 'b', 'c' 
+    double decimal=5.2; //2.499999
+    float decimal2=5.2; //2.5
+    bool vivo=true; // true / false  . 0 e 1 
+    string nome="Bruno"; //  "mundo"
+
+    cout << "Digite o numero de vidas: ";
+    cin >> vidas;
+    cout << "Digite uma letra: ";
+    cin >> letra;
+    cout << "dinheiro: ";
+    cin >> decimal;
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    // vidas=100;
+     
+
+    cout <<"\nvidas: " << vidas << "\nletras: "<< letra << "\n" << decimal << "\n" << vivo << "\n"<< nome << "\n";
 
     return 0;
 }
