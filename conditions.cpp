534612a516b2586f2e0c2a13b2e8d068d6c86372
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-07 01:14:50 -0300

    feat: condition if / else what powered

---
+++ conditions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num1=10;
+    int num2=50;
+    int sol=1;
+    char opc='s';
+    
+  if (sol){
+    cout << "Eu vou para a escola!";
+  }else{
+    cout << "Vou fazer uma viagem";
+  }
+
+return 0;
+}
