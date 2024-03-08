77add22616ececd825991c1a754f4a13c1b474dc
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-08 02:23:55 -0300

    feat: operators and,or ,not, progressive.

---
+++ opOrand.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+
+    num=1;
+
+    if(num > 4 && num <7){
+        cout << "Valor aceito\n";
+    }else{
+        cout << "Valor rejeitado\n";
+    }
+
+     if(num > 4 || num <7){
+        cout << "Valor aceito\n";
+    }else{
+        cout << "Valor rejeitado\n";
+    }
+
+    // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+    // 3 a 6
+    // 10 a 14
+    // 16 a 19
+
+    if(num > 2 && num < 7 || num > 9 && num < 15 || num > 17 && num < 20){
+        cout << "Valor aceito\n";
+    }else{
+        cout << "Valor rejeitado\n";
+    }
+
+    if ((num >= 3 && num <=6)  || (num >= 10 && num <=14) || (num >= 16 && num <=19)){
+       cout << "Valor aceito\n";
+    }else{
+        cout << "Valor rejeitado\n";
+    }
+    
+    // NEGAÇÃO
+    if (!num)
+    {
+       cout << "Eu vou para praia\n";
+    }else{
+        cout << "Eu vou ao clube\n";
+    }
+
+    //if (num == 1)  // if(num)
+    
+
+
+    return 0;
+}
