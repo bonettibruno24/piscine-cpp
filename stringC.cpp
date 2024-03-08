f6c5ceaa8372a599b417b3bee10f9322ee2192a3
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-08 01:23:23 -0300

    feat: studying arrays list in cpp

---
+++ stringC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string cars[4] = {"Volks","BMW","Ford","Motor"};
+
+    cout << cars[0] << "\n\n";
+    cout << cars[1] << "\n\n";
+    cout << cars[2] << "\n\n";
+    cout << cars[3] << "\n\n";
+
+    return 0;
+}
