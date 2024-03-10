6fceafd7578ea3cd071a8c344a6a9658b7c753be
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-10 20:53:41 -0300

    feat simple aplication utilizing break.

---
+++ Comands/break.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int cont;
+
+cont=0;
+
+while (cont < 1000)
+{
+    cout << cont << "\n\n";
+    cont++;
+    if(cont == 500){
+        break;
+    }
+}
+
+
+    return 0;
+}
