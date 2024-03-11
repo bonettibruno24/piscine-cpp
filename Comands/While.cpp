893b9a43c71f071a3f36f96fb23ce675e851806d
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-10 21:23:47 -0300

    feat: command loop do while

--- Comands/While.cpp
+++ Comands/While.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 
-    //FORMA DE UTILIZAR O WHILE
+    // FORMA DE UTILIZAR O WHILE
 
-int n;
-n=0;
-while(n<10){
-    cout << n << "\n";
-    n++;
-    
-}
-cout << "\n Rotina Finalizada\n";
- 
-
-  int cont;
-  cont=0;
-  
-    while(cont++<20){
-        cout << "Testando sequencia - " << cont << "\n" ;
-        if(cont == 10){
-            break;
-        }
+    int n;
+    n=0;
+    while(n<10){
+        cout << n << "\n";
+        n++;
     }
     cout << "\n Rotina Finalizada\n";
 
+      int cont;
+      cont=0;
+
+        while(cont++<20){
+            cout << "Testando sequencia - " << cont << "\n" ;
+            if(cont == 10){
+                break;
+            }
+        }
+        cout << "\n Rotina Finalizada\n";
 
 
-return 0;
+//UTILIZAÇÃO COMANDO DE LOOP DO WHILE;
+    int cont;
+    cont = 20;
 
+    do{
+        cout << "Testando sequencia - " << cont << "\n";
+        cont++;
+    } while (cont < 25);
+    cout << "\n Rotina Finalizada\n";
+    return 0;
 }
