2d33fb8e146799027b73732ab33493fbed78c551
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-18 19:24:41 -0300

    feat: pass arguments and vector argv ->

---
+++ Comands/argumentsF.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+//  PRIMEIRO PARAMETRO ARMAZENA A QUANTIDADE DE ARGUMENTOS (QUANTIDADE DE PARAMETROS), E SEGUNDO ARMAZENA OS ARGUMENTOS.
+int main(int argc, char *argv[]) {
+    // Verifica se pelo menos um argumento foi passado
+    if (argc > 1) {
+        if (!strcmp(argv[1], "carro")) {
+            cout << "Vai de carro \n\n";
+        } else if (!strcmp(argv[1], "moto")) {
+            cout << "Vai de moto \n\n";
+        } else {
+            cout << "Vou de ape mesmo \n\n";
+        }
+    } else {
+        cout << "Nenhum argumento passado!\n";
+    }
+    system("pause");
+    
+    return 0;
+}
