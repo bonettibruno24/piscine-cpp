536884b46e1f8f4469602b88b7dc2959b0c97940
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-17 22:42:59 -0300

    feat: comment aplication request the cond.

--- Comands/force.cpp
+++ Comands/force.cpp
@@ -17,13 +17,17 @@ int main(){
     cin >> palavras;
     system("cls");
 
+
+    // ALOCANDO A PALAVRA SECRETA NO INDICIE 0 E REALIZANDO O TAMANHO DA PALAVRA
     while(palavras[i] != '\0'){
         i++;
         tam++;
     }
+   // PARA PALAVRA SECRETA É APRESENTADO COM TRACO 
     for(i=0; i<30; i++){
         secret[i] ='-';
     }
+    // inicio do loop de jogo. VERIFICA SE O NUMERO DE CHANCES É MAIOR QUE ZERO, E VERIIFICA NUMERO DE ACERTOS É  MENOR QUE O TAMANHO DA PALAVRA SECRETA.
     while((chances > 0) && (acertos < tam)){
         cout << "chances: " << chances << "\n\n";
         cout << "Palavra secreta: " ;
@@ -31,6 +35,7 @@ int main(){
             cout << secret[i];
 
         }
+       // SE A PALAVRA NO INDICIE 0 DA PALAVRA SECRETA FOR IGUAL A LETRA DIGITADA O ACERTO É VERDADEIRO,  E O INDICIE 0 DA PALAVRA SECRETA RECEBE A LETRA CONTANDO COMO ACERTO.
         cout << "\n\nDigite uma letra: ";
         cin >> letras[0];
         for(i=0; i<tam; i++){
@@ -40,10 +45,12 @@ int main(){
                 acertos++;
             }
         }
+        //SE VOCE NAO ACERTOU A LETRA DEVE DIMINUIR O NUMERO DE CHANCES
         if(!acerto){
             chances --;
 
         }
+       // CASO O ACERTO SEJA FALSO ELE LIMPA O SIISTEMA E EMITE UM AVISO PARA O USUARIO PARA TENTAR NOVAMENTE. CAINDO NA CONDIÇÃO FIINAL.
         acerto=false;
         system("cls");
     }
