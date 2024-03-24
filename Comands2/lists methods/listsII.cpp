0edb61cb53fcab305d072f3caf2a618720054ef9
!--2B--! <brunobonetti.silva@gmail.com>
2024-03-24 04:36:07 -0300

    feat: parameters the list remove, add, insert

---
+++ Comands2/lists methods/listsII.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(){
+
+    list <int> aula;
+    int tam;
+    list<int>::iterator it; //DECLARAÇÃO DO INTERATOR
+
+    tam=10;
+    for(int i=0; i<tam; i++){
+        aula.push_front(i); //IMPRIME A LISTA DE TRAS PRA FRENTE
+    }
+
+    it=aula.begin();
+    advance(it, 5); //POSIÇÃO PARA INICIAR A BUSCA
+
+    aula.insert(it, 0); //VALOR A SER INSERIDO
+
+    aula.sort(); //ORDEM CRESCENTE
+    aula.reverse(); //ORDEM DECRESCENTE
+
+    cout << "Tamanho da lista: " << aula.size() << "\n";
+
+    tam=aula.size();
+    for(int i=0; i<tam; i++){
+        cout << aula.front() << "\n";
+        aula.pop_front(); //RETIRA O PRIMEIRO ELEMENTO
+    }
+
+    return 0;
+}
